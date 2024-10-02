98c4c781afc3eb72f5ce47e1e0fa9269f2562915
Gyanranjan Patra 👾 <153155648+Gyannnnn@users.noreply.github.com>
2024-10-02 16:27:09 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['smartslider3.com'], // Add the hostname here
+    },
+  };
+  
+  export default nextConfig;
+  
